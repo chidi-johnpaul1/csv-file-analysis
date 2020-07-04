e054a35f4dfa695d3a7b7449e004667d3489950c
Okpala Chidi J <46085705+chidi-johnpaul1@users.noreply.github.com>
2020-07-04 11:44:28 +0200

    Add files via upload

---
+++ SalesRecordAnalysis/src/com/salesrecordanalysis/SalesRecordAnalysis.java
@@ -0,0 +1,17 @@
+
+package com.salesrecordanalysis;
+
+/**
+ *
+ * @author CHIDI
+ */
+public class SalesRecordAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
